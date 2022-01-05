dd2786f1949088d2997fd25ac223370c06b6ac1f
Yannael_LB <>
2022-01-05 12:01:14 +0100

    Fixed wording

--- Chapter_References/shared_functions.py
+++ Chapter_References/shared_functions.py
@@ -143,11 +143,11 @@ def get_train_test_set(transactions_df,
     # Get the test set data
     test_df = []
     
-    # Note: Cards known to be frauded after the delay period are removed from the test set
+    # Note: Cards known to be compromised after the delay period are removed from the test set
     # That is, for each test day, all frauds known at (test_day-delay_period) are removed
     
-    # First, get known frauded customers from the training set
-    known_frauded_customers = set(train_df[train_df.TX_FRAUD==1].CUSTOMER_ID)
+    # First, get known defrauded customers from the training set
+    known_defrauded_customers = set(train_df[train_df.TX_FRAUD==1].CUSTOMER_ID)
     
     # Get the relative starting day of training set (easier than TX_DATETIME to collect test data)
     start_tx_time_days_training = train_df.TX_TIME_DAYS.min()
@@ -160,15 +160,15 @@ def get_train_test_set(transactions_df,
                                                                     delta_train+delta_delay+
                                                                     day]
         
-        # Frauded cards from that test day, minus the delay period, are added to the pool of known frauded customers
+        # Compromised cards from that test day, minus the delay period, are added to the pool of known defrauded customers
         test_df_day_delay_period = transactions_df[transactions_df.TX_TIME_DAYS==start_tx_time_days_training+
                                                                                 delta_train+
                                                                                 day-1]
         
-        new_frauded_customers = set(test_df_day_delay_period[test_df_day_delay_period.TX_FRAUD==1].CUSTOMER_ID)
-        known_frauded_customers = known_frauded_customers.union(new_frauded_customers)
+        new_defrauded_customers = set(test_df_day_delay_period[test_df_day_delay_period.TX_FRAUD==1].CUSTOMER_ID)
+        known_defrauded_customers = known_defrauded_customers.union(new_defrauded_customers)
         
-        test_df_day = test_df_day[~test_df_day.CUSTOMER_ID.isin(known_frauded_customers)]
+        test_df_day = test_df_day[~test_df_day.CUSTOMER_ID.isin(known_defrauded_customers)]
         
         test_df.append(test_df_day)
         
@@ -210,11 +210,11 @@ def get_train_delay_test_set(transactions_df,
     # Get the test set data
     test_df = []
     
-    # Note: Cards known to be frauded after the delay period are removed from the test set
+    # Note: Cards known to be compromised after the delay period are removed from the test set
     # That is, for each test day, all frauds known at (test_day-delay_period) are removed
     
-    # First, get known frauded customers from the training set
-    known_frauded_customers = set(train_df[train_df.TX_FRAUD==1].CUSTOMER_ID)
+    # First, get known defrauded customers from the training set
+    known_defrauded_customers = set(train_df[train_df.TX_FRAUD==1].CUSTOMER_ID)
     
     # Get the relative starting day of training set (easier than TX_DATETIME to collect test data)
     start_tx_time_days_training = train_df.TX_TIME_DAYS.min()
@@ -227,15 +227,15 @@ def get_train_delay_test_set(transactions_df,
                                                                     delta_train+delta_delay+
                                                                     day]
         
-        # Frauded cards from that test day, minus the delay period, are added to the pool of known frauded customers
+        # Compromised cards from that test day, minus the delay period, are added to the pool of known defrauded customers
         test_df_day_delay_period = transactions_df[transactions_df.TX_TIME_DAYS==start_tx_time_days_training+
                                                                                 delta_train+
                                                                                 day-1]
         
-        new_frauded_customers = set(test_df_day_delay_period[test_df_day_delay_period.TX_FRAUD==1].CUSTOMER_ID)
-        known_frauded_customers = known_frauded_customers.union(new_frauded_customers)
+        new_defrauded_customers = set(test_df_day_delay_period[test_df_day_delay_period.TX_FRAUD==1].CUSTOMER_ID)
+        known_defrauded_customers = known_defrauded_customers.union(new_defrauded_customers)
         
-        test_df_day = test_df_day[~test_df_day.CUSTOMER_ID.isin(known_frauded_customers)]
+        test_df_day = test_df_day[~test_df_day.CUSTOMER_ID.isin(known_defrauded_customers)]
         
         test_df.append(test_df_day)
         
@@ -357,12 +357,12 @@ def card_precision_top_k_day(df_day,top_k):
             
     # Get the top k most suspicious cards
     df_day_top_k=df_day.head(top_k)
-    list_detected_frauded_cards=list(df_day_top_k[df_day_top_k.TX_FRAUD==1].CUSTOMER_ID)
+    list_detected_compromised_cards=list(df_day_top_k[df_day_top_k.TX_FRAUD==1].CUSTOMER_ID)
     
     # Compute precision top k
-    card_precision_top_k = len(list_detected_frauded_cards) / top_k
+    card_precision_top_k = len(list_detected_compromised_cards) / top_k
     
-    return list_detected_frauded_cards, card_precision_top_k
+    return list_detected_compromised_cards, card_precision_top_k
 
 
 # ### card_precision_top_k
@@ -373,17 +373,17 @@ def card_precision_top_k_day(df_day,top_k):
 # In[ ]:
 
 
-def card_precision_top_k(predictions_df, top_k, remove_detected_frauded_cards=True):
+def card_precision_top_k(predictions_df, top_k, remove_detected_compromised_cards=True):
 
     # Sort days by increasing order
     list_days=list(predictions_df['TX_TIME_DAYS'].unique())
     list_days.sort()
     
-    # At first, the list of detected frauded cards is empty
-    list_detected_frauded_cards = []
+    # At first, the list of detected compromised cards is empty
+    list_detected_compromised_cards = []
     
     card_precision_top_k_per_day_list = []
-    nb_frauded_cards_per_day = []
+    nb_compromised_cards_per_day = []
     
     # For each day, compute precision top k
     for day in list_days:
@@ -391,24 +391,24 @@ def card_precision_top_k(predictions_df, top_k, remove_detected_frauded_cards=Tr
         df_day = predictions_df[predictions_df['TX_TIME_DAYS']==day]
         df_day = df_day[['predictions', 'CUSTOMER_ID', 'TX_FRAUD']]
         
-        # Let us remove detected frauded cards from the set of daily transactions
-        df_day = df_day[df_day.CUSTOMER_ID.isin(list_detected_frauded_cards)==False]
+        # Let us remove detected compromised cards from the set of daily transactions
+        df_day = df_day[df_day.CUSTOMER_ID.isin(list_detected_compromised_cards)==False]
         
-        nb_frauded_cards_per_day.append(len(df_day[df_day.TX_FRAUD==1].CUSTOMER_ID.unique()))
+        nb_compromised_cards_per_day.append(len(df_day[df_day.TX_FRAUD==1].CUSTOMER_ID.unique()))
         
-        detected_frauded_cards, card_precision_top_k = card_precision_top_k_day(df_day,top_k)
+        detected_compromised_cards, card_precision_top_k = card_precision_top_k_day(df_day,top_k)
         
         card_precision_top_k_per_day_list.append(card_precision_top_k)
         
-        # Let us update the list of detected frauded cards
-        if remove_detected_frauded_cards:
-            list_detected_frauded_cards.extend(detected_frauded_cards)
+        # Let us update the list of detected compromised cards
+        if remove_detected_compromised_cards:
+            list_detected_compromised_cards.extend(detected_compromised_cards)
         
     # Compute the mean
     mean_card_precision_top_k = np.array(card_precision_top_k_per_day_list).mean()
     
     # Returns precision top k per day as a list, and resulting mean
-    return nb_frauded_cards_per_day,card_precision_top_k_per_day_list,mean_card_precision_top_k
+    return nb_compromised_cards_per_day,card_precision_top_k_per_day_list,mean_card_precision_top_k
 
 
 # ### card_precision_top_k_custom
@@ -426,7 +426,7 @@ def card_precision_top_k_custom(y_true, y_pred, top_k, transactions_df):
     predictions_df['predictions']=y_pred
     
     # Compute the CP@k using the function implemented in Chapter 4, Section 4.2
-    nb_frauded_cards_per_day,card_precision_top_k_per_day_list,mean_card_precision_top_k=        card_precision_top_k(predictions_df, top_k)
+    nb_compromised_cards_per_day,card_precision_top_k_per_day_list,mean_card_precision_top_k=        card_precision_top_k(predictions_df, top_k)
     
     # Return the mean_card_precision_top_k
     return mean_card_precision_top_k
@@ -929,11 +929,11 @@ def get_tx_stats(transactions_df, start_date_df="2018-04-01"):
     #Number of fraudulent transactions per day
     nb_fraudulent_transactions_per_day=transactions_df.groupby(['TX_TIME_DAYS'])['TX_FRAUD'].sum()
     #Number of fraudulent cards per day
-    nb_frauded_card_per_day=transactions_df[transactions_df['TX_FRAUD']==1].groupby(['TX_TIME_DAYS']).CUSTOMER_ID.nunique()
+    nb_compromised_card_per_day=transactions_df[transactions_df['TX_FRAUD']==1].groupby(['TX_TIME_DAYS']).CUSTOMER_ID.nunique()
     
     tx_stats=pd.DataFrame({"nb_tx_per_day":nb_tx_per_day,
                            "nb_fraudulent_transactions_per_day":nb_fraudulent_transactions_per_day,
-                           "nb_frauded_cards_per_day":nb_frauded_card_per_day})
+                           "nb_compromised_cards_per_day":nb_compromised_card_per_day})
 
     tx_stats=tx_stats.reset_index()
     
@@ -972,7 +972,7 @@ def get_template_tx_stats(ax ,fs,
     plt.xticks(fontsize=fs*0.7)    
 
     ax.axvline(start_date_training+datetime.timedelta(days=delta_train), 0,ylim, color="black")
-    ax.axvline(start_date_test, 0, ylim, color="black")
+    ax.axvline(start_date_training+datetime.timedelta(days=delta_train+delta_delay), 0, ylim, color="black")
     
     ax.text(start_date_training+datetime.timedelta(days=2), ylim-20,'Training period', fontsize=fs)
     ax.text(start_date_training+datetime.timedelta(days=delta_train+2), ylim-20,'Delay period', fontsize=fs)
